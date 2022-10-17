8c1f85f7ed6dbd5d3317a5f98cc1a860a92cd49c
Nidhi Jain <nidhijain@Nidhis-MacBook-Air.local>
2022-10-17 19:21:53 +0200

    Removed duplicate readme file

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# timer_assignment
--- src/MultiProcessingController.py
+++ src/MultiProcessingController.py
@@ -3,9 +3,7 @@ from multiprocessing import Process
 
 def spawn_new_process(func_name, args):
     process = Process(target=func_name, args=args)
-    # run the process
     process.start()
-    # wait for the process to finish
-    print('Waiting for the process...')
+    print('Started a new process...')
     process.join()
 
